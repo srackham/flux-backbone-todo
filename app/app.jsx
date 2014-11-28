f6cbfae889deb150718ee2286ae2d68f286ccf43
Stuart Rackham <srackham@gmail.com>
2014-11-28 21:20:32 +1300

    Persist Todo list in browser localStorage.

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,7 @@
 var React = require('react');
 var Flux = require('flux');
 var Backbone = require('backbone');
+var LocalStorage = require('./localstorage.js');
 
 /*
  Dispatcher actions.
@@ -31,24 +32,40 @@ var TodoItem = Backbone.Model.extend({
  */
 var TodoStore = Backbone.Collection.extend({
   model: TodoItem,
+  localStorage: new LocalStorage('flux-backbone-todo'),
 
   initialize: function(models, options) {
     this.dispatcher = options.dispatcher;
     this.dispatchId = this.dispatcher.register(this.dispatchCallback.bind(this));
+    this.on('sync', function(model, resp) {
+      console.log('SUCCESS: sync response:', resp);
+    });
+    this.on('error', function(model, resp) {
+      console.log('ERROR: sync error:', resp);
+    });
+    // Load models from localStorage and attach the dispatcher.
+    this.fetch();
+    this.forEach(function(todoItem) { todoItem.dispatcher = options.dispatcher; });
   },
 
   dispatchCallback: function(payload) {
     switch (payload.action) {
       case ADD_TODO:
-        this.add({text: payload.text}, {dispatcher: this.dispatcher});
+        var todoItem = this.add({text: payload.text}, {dispatcher: this.dispatcher});
+        todoItem.save();
         break;
       case TOGGLE_TODO:
         payload.todoItem.set('complete', !payload.todoItem.get('complete'));
+        payload.todoItem.save();
         break;
       case CLEAR_TODOS:
-        this.reset(this.filter(function(todoItem) {
-          return !todoItem.get('complete');
-        }));
+        var completed = this.filter(function(todoItem) {
+          return todoItem.get('complete');
+        });
+        // Destroy todoItem before removing from collection.
+        // Because removed models have no collection property which is required by LocalStorage.
+        completed.forEach(function(todoItem) { todoItem.destroy() });
+        this.remove(completed);
         break;
     }
   }
@@ -156,7 +173,7 @@ var TodoItemComponent = React.createClass({
  Instantiate Todo application.
  */
 var dispatcher = new Flux.Dispatcher();
-var todoStore = new TodoStore([], {dispatcher: dispatcher});
+var todoStore = new TodoStore(null, {dispatcher: dispatcher});
 
 React.render(
   <div>
---
+++ app/localstorage.js
@@ -0,0 +1,120 @@
+// A simple CommonJS module to replace Backbone.sync` with browser localStorage.
+//
+// https://github.com/addyosmani/backbone-fundamentals/blob/gh-pages/practicals/modular-todo-app/js/libs/backbone/localstorage.js
+//
+// Stuart Rackham: November 2014:
+// Converted to a node compatible module for use by Browserify/Webpack;
+// removed Underscore dependency; comments and refactoring.
+//
+
+'use strict';
+
+var Backbone = require('backbone');
+
+// Constructor function for creating Backbone sync adaptor objects.
+var LocalStorage = function(name) {
+  this.name = name;
+  var json = window.localStorage.getItem(this.name);
+  // data is keyed by model model id and contains model attribute hashes.
+  this.data = (json && JSON.parse(json)) || {};
+};
+module.exports = LocalStorage;
+
+var localStorageFunctions = {
+
+  saveData: function() {
+    window.localStorage.setItem(this.name, JSON.stringify(this.data));
+  },
+
+  create: function(model) {
+    if (!model.id) {
+      model.id = model.attributes.id = guid();
+    }
+    return this.update(model);
+  },
+
+  update: function(model) {
+    this.data[model.id] = model.toJSON();
+    this.saveData();
+    return model.toJSON();
+  },
+
+  find: function(model) {
+    return this.data[model.id];
+  },
+
+  findAll: function() {
+    // Return array of model attribute hashes.
+    return Object.keys(this.data).map(
+      function(id) {
+        return this.data[id];
+      }.bind(this)
+    );
+  },
+
+  destroy: function(model) {
+    delete this.data[model.id];
+    this.saveData();
+    return model.toJSON();
+  }
+
+};
+for (var name in localStorageFunctions) {
+  LocalStorage.prototype[name] = localStorageFunctions[name];
+}
+
+// Generate four random hex digits.
+function s4() {
+  return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+}
+
+// Generate a pseudo-GUID by concatenating random hexadecimal.
+function guid() {
+  return (s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4());
+}
+
+/*
+ Overrides Backbone.sync function.
+
+ Called by Backbone Model fetch, save and destroy APIs (with the `model` argument set
+ to the bound Model) and by the Collection fetch API (with the `model` argument set
+ to the bound Collection).
+
+ When processing a collection it's important that each model has a valid `collection` property.
+ This will automatically be the case unless the model has been removed from or is not
+ contained in a collection.
+
+ Backbone exposes a success/error style callback interface to the fetch, save, and destroy APIs.
+ This is to accommodate asynchronous client/server interactions e.g. using AJAX. But because
+ all localStorage requests synchronously the fetch, save and destroy APIs behave synchronous.
+
+ */
+Backbone.sync = function(method, model, options) {
+  console.log('sync', method, model);
+  var localStorage = model.localStorage || model.collection.localStorage;
+  var resp; // JSON response from the "server".
+  switch (method) {
+    case 'read':    // Model/Collection `fetch` APIs.
+      resp = model.id ? localStorage.find(model) : localStorage.findAll();
+      break;
+    case 'create':  // Model `save` API.
+      resp = localStorage.create(model);
+      break;
+    case 'update':  // Model `save` API.
+      resp = localStorage.update(model);
+      break;
+    case 'delete':  // Model `destroy` API.
+      resp = localStorage.destroy(model);
+      break;
+  }
+  if (resp) {
+    // 1. If necessary update the model/collection.
+    // 2. Execute the fetch/save/destroy `options.success` callback.
+    // 3. Emit a "sync" event with arguments model,resp,options.
+    options.success(resp);
+  } else {
+    // 1. Execute the fetch/save/destroy `options.error` callback.
+    // 2. Emit an "error" event with arguments model,resp,options.
+    options.error(method);
+  }
+};
